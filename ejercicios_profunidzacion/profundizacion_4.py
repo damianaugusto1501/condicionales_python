54bf726bfd71bc7731dc60565a86739aab3dc7c8
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-26 14:42:28 -0300

    Update profundizacion_4.py

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -33,3 +33,67 @@ contenido.
 
 print('Ejercicios de práctica con cadenas')
 # Empezar aquí la resolución del ejercicio
+
+Orden_Alfabetico_1 = str
+Orden_Alfabetico_2 = str
+Orden_Alfabetico_3 = str
+Orden_de_longitud_1 = str
+Orden_de_longitud_2 = str
+Orden_de_longitud_3 = str
+
+print("Ingrese la primer palabra")
+palabra_1 = str(input())
+
+Orden_Alfabetico_1 = palabra_1
+Orden_de_longitud_1 = palabra_1
+
+print("Ingrese la segunda palabra")
+palabra_2 = str(input())
+
+if palabra_2[0] < Orden_Alfabetico_1[0]:
+    Orden_Alfabetico_1 = palabra_2
+else :
+    Orden_Alfabetico_2 = palabra_2
+
+if len(palabra_2) > len(Orden_Alfabetico_1) :
+    Orden_de_longitud_1 = palabra_2
+else :
+    Orden_de_longitud_2 = palabra_2
+
+
+print("Ingrese la tercer palabra")
+palabra_3 = str(input())
+
+
+if palabra_3[0] < Orden_Alfabetico_2 [0] :
+    Orden_Alfabetico_2 = palabra_3
+    Orden_Alfabetico_3 = palabra_2
+else :
+    Orden_Alfabetico_3 = palabra_3
+
+if palabra_3[0] < Orden_Alfabetico_2[0] and palabra_3[0] < Orden_Alfabetico_3[0] :
+    Orden_Alfabetico_3 = Orden_Alfabetico_2
+    Orden_Alfabetico_2 = Orden_Alfabetico_1
+    Orden_Alfabetico_1 = palabra_3
+
+if len(palabra_3) > len(Orden_de_longitud_2) :
+    Orden_de_longitud_2 = palabra_3
+    Orden_de_longitud_3 = palabra_2
+else :
+    Orden_de_longitud_3 = palabra_3
+
+if len(palabra_3) > len(palabra_2) and len(palabra_3) > len(palabra_1) :
+    Orden_de_longitud_3 = Orden_Alfabetico_2
+    Orden_de_longitud_2 = Orden_de_longitud_1
+    Orden_de_longitud_1 = palabra_3
+
+
+
+
+print("Ingrese como quiere ordenar , Alfabeticamente : 1 , Por cantidad de letras : 2 ")
+Opcion_para_elegir = int(input())
+
+if Opcion_para_elegir == 1 :
+    print("el orden alfabetico es : {} ,{} ,{}" .format(Orden_Alfabetico_1,Orden_Alfabetico_2,Orden_Alfabetico_3))
+else :
+    print("El Orden por longitud de palabra es {} , {} , {}:" .format(Orden_de_longitud_1,Orden_de_longitud_2,Orden_de_longitud_3))
