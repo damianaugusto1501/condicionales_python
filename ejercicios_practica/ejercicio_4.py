451b98cb366e853508e32a11f275b400f9d6914f
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-26 14:37:18 -0300

    Update ejercicio_4.py

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,12 +17,27 @@ texto_2 = '7'
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
 
+if texto_1 < texto_2 :
+    print(texto_1,"es mayor alfabeticamente a ",texto_2)
+else :
+    print(texto_2,"es mayor alfabeticamente a ",texto_1)
+
+
+
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
 # Compare las nuevas variables para ver cual es mayor o menor
 # utilizando los operadores correspondientes
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+texto_variable1 = int(texto_1)
+texto_variable2 = int(texto_2)
+
+if texto_variable1 > texto_variable2 :
+    print(texto_variable1, "es mayor numericamente")
+else :
+    print(texto_2, "es mayor numericamente")
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
