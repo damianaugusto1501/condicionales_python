f9003d1f194827744257a1a1304b8b8d6af84d59
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-26 14:36:40 -0300

    Update ejercicio_2.py

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -18,6 +18,71 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
+if texto_1 < texto_2:
+    print("{} es mayor a {} ".format(texto_1,texto_2))
+else :
+    print("{} es mayor a {} " .format(texto_2,texto_1))
+
+
+
+# Compare cual de las dos palabras tiene mayor
+# cantidad de letras
+# Imprima en pantalla según corresponda
+
+if (len(texto_1) > len(texto_2)) :
+    print(texto_1," tiene mas cantidad de letras ")
+
+
+# Verifique si la primera letra de la primera palabra
+# es mayor a la primera letra de la segunda palabra
+# Imprima en pantalla según corresponda
+
+if (texto_1[0] > texto_2[0]) :
+    print("la primera letra de ",texto_1,"es mayor a la letra de ",texto_2)
+else :
+    print("la primera letra de ",texto_2,"es mayor a la letra de ",texto_1)
+
+
+  # Copia de la variable texto_1
+copia_texto1 = texto_1
+
+# Verifique que copia_texto_1 es igual Aa texto_1
+# Imprima en pantalla según corresponda
+if (copia_texto1 == texto_1):
+    print("copia verificada con exito")
+else :
+    print("error en verificacion")
+
+# Verifique que copia_texto_1 es distinta a texto_2
+# Imprima en pantalla según corresponda
+
+if (copia_texto1 != texto_2):
+    print("copia de texto distinta a texto 2")
+else:
+    print("error de distincion")
+
+
+
+# Condicionales [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Comparadores
+# Ingrese dos palabras cualesquiera y realice las sigueintes
+# comparaciones entre ellas
+texto_1 = str(input('Ingrese la primera palabra:\n'))
+
+texto_2 = str(input('Ingrese la segunda palabra:\n'))
+
+# Compare cual de las dos palabras es mayor (alfabéticamente)
+# Imprima en pantalla según corresponda
 
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
