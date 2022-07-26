fcfeabc87fc1ef45d2712c4af12e85955bd9a270
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-26 14:36:17 -0300

    Update ejercicio_1.py

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -7,59 +7,48 @@
 # IMPORTANTE: NO borrar los comentarios
 # que aparecen en verde con el hashtag "#"
 
-# Ejemplos variables de texto
+# Ejercicios de práctica numérica
 
 # Comparadores
-# Ingrese dos palabras cualesquiera y realice las sigueintes
-# comparaciones entre ellas
-texto_1 = str(input('Ingrese la primera palabra:\n'))
+# Ingrese dos números cualesquiera y realice las sigueintes
+# comparaciones entre ellos
+numero_1 = int(input('Ingrese el primer número:\n'))
 
-texto_2 = str(input('Ingrese la segunda palabra:\n'))
+numero_2 = int(input('Ingrese el segundo número:\n'))
 
-# Compare cual de las dos palabras es mayor (alfabéticamente)
+# Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
-if texto_1 < texto_2:
-    print("{} es mayor a {} ".format(texto_1,texto_2))
+
+if (numero_1 > numero_2) :
+    print(numero_1," es mayor que ", numero_2)
 else :
-    print("{} es mayor a {} " .format(texto_2,texto_1))
+    print(numero_2," es mayor que ", numero_1)
 
+# Verifique si el numero_1 positivo, negativo o cero
+# Imprima el resultado en cada caso
 
-
-# Compare cual de las dos palabras tiene mayor
-# cantidad de letras
-# Imprima en pantalla según corresponda
-
-if (len(texto_1) > len(texto_2)) :
-    print(texto_1," tiene mas cantidad de letras ")
-
-
-# Verifique si la primera letra de la primera palabra
-# es mayor a la primera letra de la segunda palabra
-# Imprima en pantalla según corresponda
-
-if (texto_1[0] > texto_2[0]) :
-    print("la primera letra de ",texto_1,"es mayor a la letra de ",texto_2)
+if (numero_1 > 0) :
+    print("el numero ", numero_1 , " es positivo ")
+elif (numero_1 < 0) :
+    print("el numero " , numero_1 , "es negativo")
 else :
-    print("la primera letra de ",texto_2,"es mayor a la letra de ",texto_1)
+    print ("el numero ", numero_1 , "es 0")
 
 
-  # Copia de la variable texto_1
-copia_texto1 = texto_1
 
-# Verifique que copia_texto_1 es igual Aa texto_1
-# Imprima en pantalla según corresponda
-if (copia_texto1 == texto_1):
-    print("copia verificada con exito")
+# Verifique si el numero_1 es mayor a 0 y menor a 100
+# Imprima en pantalla si se cumple o no la condición
+if  (numero_1 > 0 and numero_1 < 100) :
+    print("el numero ", numero_1 , " es mayor a 0 y menor a 100 ")
+    print("condicion cumplida")
 else :
-    print("error en verificacion")
+    print ("condicion no cumplida")
 
-# Verifique que copia_texto_1 es distinta a texto_2
-# Imprima en pantalla según corresponda
+# Verifique si el numero_1 es menor a 10 o el numero_2
+# es mayor a -2
+# Imprima en pantalla si se cumple o no la condición
 
-if (copia_texto1 != texto_2):
-    print("copia de texto distinta a texto 2")
-else:
-    print("error de distincion")
-
+if (numero_1 < 10 or numero_2 > -2 ) :
+    print ("la condicion se cumple")
 
 
