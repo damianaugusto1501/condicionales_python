14961fd35db0e6d903c97b5921d63dfd33665c1f
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-26 14:41:57 -0300

    Update profundizacion_3.py

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -27,6 +27,44 @@ IMPORTANTE: Para ordenar las temperatuas debe utilizar condicionales compuestos 
 no se busca utilizar bucles o algoritmos de ordenamiento ya que aún no hemos llegado a ese
 contenido. Recomendamos pensar bien este problema de lógica con un lápiz y papel.
 '''
+temperatura_Maxima = float
+temperatura_minima = float
+Promedio_temperatura = float
+
+print("Ingrese la primer temperatura :")
+temperatura_1 = float(input())
+temperatura_Maxima = temperatura_1
+temperatura_minima = temperatura_1
+
+print("Ingrese la segunda temperatura :")
+temperatura_2 = float(input())
+
+if (temperatura_2 > temperatura_1) :
+    temperatura_Maxima = temperatura_2 # esta mal escribir los IFs entre inputs?
+
+if (temperatura_2 < temperatura_minima) :
+    temperatura_minima = temperatura_2
+
+print("Ingrese la tercer temperatura :")
+temperatura_3 = float(input())
+
+if (temperatura_3 > temperatura_Maxima) :
+    temperatura_Maxima = temperatura_3
+
+if (temperatura_3 < temperatura_minima) :
+    temperatura_minima = temperatura_3
+
+Promedio_temperatura = temperatura_1 + temperatura_2 + temperatura_3 / 3
+
+print("La temperatura maxima registrada fue de : " ,temperatura_Maxima,"Grados")
+print("La temperatura minima registrada fue de : " ,temperatura_minima,"Grados")
+print("El promedio de temperaturas fue de :" ,Promedio_temperatura, "Grados")
+
+
+
+
+
+
 
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
