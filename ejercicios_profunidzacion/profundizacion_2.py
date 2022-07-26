49a149d8694a4514a285d3a117160d5df5c4fb7f
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-26 14:40:34 -0300

    Update profundizacion_2.py

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -20,6 +20,30 @@ enteros, y luego en cada caso informe si el número es par
 o impar.
 Para cada caso imprimir el resultado en pantalla.
 '''
+print("ingrese el primer numero")
+numero_1 = float(input())   # esta mal ponerlo como float por si la persona ingresa numeros con decimales?
+print("ingrese el segundo numero") 
+numero_2 = float(input())
+print("ingrese el tercer numero")
+numero_3 = float(input())
+
+if numero_1 % 2 == 0 :
+    print(numero_1, "Es un numero par")
+else :
+    print(numero_1, "Es un numero impar")
+    
+if numero_2 % 2 == 0 :
+    print(numero_2, "Es un numero par")
+else :
+    print(numero_2, "Es un numero impar")
+
+if numero_3 % 2 == 0 :
+    print(numero_3, "Es un numero par")
+else :
+    print(numero_3, "Es un numero impar")
+
+
+
 
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
